8fbfacae1ddbf800296109ae808dc9f09da6d54b
Marcio José Lisboa <marcio.lisboa@philips.com>
2022-03-16 14:01:53 -0300

    Fix date

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -155,6 +155,20 @@ const updateUI = function (acc) {
 // Event handlers
 let currentAccount;
 
+
+// FAKE AWAYS LOGGED IN
+currentAccount = account1;
+updateUI(currentAccount)
+containerApp.style.opacity = 100;
+
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = now.getMinutes();
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
