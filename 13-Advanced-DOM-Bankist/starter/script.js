4c05941265062244f68d1e4949eb3eae6b8fa39c
Marcio José Lisboa <marcio.lisboa@philips.com>
2022-03-25 15:21:02 -0300

    Building the tabbed component

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -61,3 +61,26 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
   }
 })
+
+//////////////////////////
+/// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'))
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Active content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+})
