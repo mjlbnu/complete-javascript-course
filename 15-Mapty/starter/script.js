cb3d071b1e660d42d6d590d89fd1a4c091f694c3
Marcio Lisboa <mjlbnu@gmail.com>
2022-05-25 22:46:34 -0300

    Mapty app - rendering workout input form

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,32 +11,50 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude, longitude } = position.coords;
     const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    map.on('click', function (mapEvent) {
-      const { lat, lng } = mapEvent.latlng;
-
-      L.marker([lat, lng]).addTo(map)
-        .bindPopup(L.popup({
-          maxWidth: 250,
-          minWidth: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className: 'running-popup'
-        }))
-        .setPopupContent('Workout')
-        .openPopup();
+    // Handling clicks on map
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden')
+      inputDistance.focus();
     })
   },
   function () {
     alert('Could not get your position');
   });
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng]).addTo(map)
+    .bindPopup(L.popup({
+      maxWidth: 250,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+      className: 'running-popup'
+    }))
+    .setPopupContent('Workout')
+    .openPopup();
+})
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
